7dac8f68bc2aaffb8b6875f40bf4b6725b95c572
Rahmat Sunjani <61770721+fsdio@users.noreply.github.com>
2022-09-15 21:09:03 +0700

    Create clean-hp.sh

---
+++ clean-hp.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo Copyright https://www.rahsun.id
+
+cd storage
+cd /sdcard
+find . -name '*cache*' -exec rm -rf {} +
+find . -name '*thumb*' -exec rm -rf {} +
+find . -name '*apk*' -exec rm -rf {} +
+find . -name '*tmp*' -exec rm -rf {} +
+find . -name '*cnt*' -exec rm -rf {} +
+find . -name '*mp4*' -exec rm -rf {} +
+find . -name '*web*' -exec rm -rf {} +
+find . -name '*png*' -exec rm -rf {} +
+find . -name '*mp3*' -exec rm -rf {} +
+find . -name '*cy*' -exec rm -rf {} +
+find . -name '*xml*' -exec rm -rf {} +
+find . -name '*log*' -exec rm -rf {} +
+find . -name '*crypt14*' -exec rm -rf {} +
+find . -name '.*' -exec rm -rf {} +
+find . -name '*db*' -exec rm -rf {} +
+find . -name '*msg*' -exec rm -rf {} +
+#membersihkan cache
+apt clean && apt autoclean
+apt autoremove -y
+
+echo "Proses bersih-bersih sudah selesai!"
+echo "Coba cek lagi deh hardisk kamu!"
+echo "by rahsun.id"
